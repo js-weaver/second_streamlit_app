598ebe70ef7cca2dcdb04467f86cfb2c5fd01785
js-weaver <126171784+js-weaver@users.noreply.github.com>
2023-03-14 15:28:36 +0200

    Update second_streamlit_app.py

--- second_streamlit_app.py
+++ second_streamlit_app.py
@@ -1,3 +1,7 @@
+import streamlit
+import snowflake.connector
+import pandas
+
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
@@ -5,10 +9,6 @@ my_data_row = my_cur.fetchone()
 streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
 
-               
-import streamlit
-import snowflake.connector
-import pandas
 streamlit.title('Zena\'s Amazing Athleisure Catalog')
 # connect to snowflake
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
